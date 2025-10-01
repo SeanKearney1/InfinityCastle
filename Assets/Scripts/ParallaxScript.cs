fa0cdd4059a55736cf97eb815b361880c79abeca
SeanKearney1 <skearney@wowway.com>
2025-10-01 00:46:47 -0400

    9/30/2025

--- Assets/Scripts/ParallaxScript.cs
+++ Assets/Scripts/ParallaxScript.cs
@@ -5,8 +5,10 @@ public class ParallaxScript : MonoBehaviour
     public float ParallaxSpeed;
     private float GameTime = 0.0F;
     public float ParallaxOffset;
+
+    public int AdditionalPanels;
     public Vector2 ParallaxDirection;
-    private Vector2 pixel_size = new Vector2(0,0);
+    private Vector2 pixel_size = new Vector2(0, 0);
     private Vector2 image_scale = new Vector2(0, 0);
     private Vector2 original_pos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,9 +26,15 @@ public class ParallaxScript : MonoBehaviour
 
         Vector2 moving_direction = new Vector2(0, 0);
 
-        moving_direction.x = original_pos.x + ((GameTime * ParallaxSpeed + ParallaxOffset) % (pixel_size.y * image_scale.y)) * ParallaxDirection.x;
-        moving_direction.y = original_pos.y + ((GameTime * ParallaxSpeed + ParallaxOffset) % (pixel_size.y * image_scale.y)) * ParallaxDirection.y;
+        moving_direction.x = GetDirection(original_pos.x, pixel_size.x, image_scale.x, ParallaxDirection.x);
+        moving_direction.y = GetDirection(original_pos.y, pixel_size.y, image_scale.y, ParallaxDirection.y);
 
         transform.position = moving_direction;
+    }
+
+
+    private float GetDirection(float og_pos, float pixel_dim, float image_dim, float direction)
+    {
+        return og_pos + ((GameTime * ParallaxSpeed + ParallaxOffset) % (pixel_dim * image_dim * AdditionalPanels)) * direction;
     }
 }
